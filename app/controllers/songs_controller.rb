2d2bf88b394ff7c8157a97f753d4e08658a4877f
jqzheng617 <jenna.zheng617@gmail.com>
2019-07-01 00:46:05 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,44 +1,10 @@
 class SongsController < ApplicationController
   def index
+    @songs = Song.all
   end
 
   def show
-  end
-
-  def new
-  end
-
-  def create
-    @song = Song.new(song_params)
-
-    if @song.save
-      redirect_to @song
-    else
-      render :new
-    end
-  end
-
-  def edit
     @song = Song.find(params[:id])
-  end
-
-  def update
-    @song = Song.find(params[:id])
-
-    @song.update(song_params)
-
-    if @song.save
-      redirect_to @song
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @song = Song.find(params[:id])
-    @song.destroy
-    flash[:notice] = "Song deleted."
-    redirect_to songs_path
   end
 
   private
@@ -47,4 +13,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title)
   end
 end
-
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,9 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def artist_name
+    #song gets the artist name
+    #song can access artist name by the belongs_to association
+    self.artist.name
+  end
 end
